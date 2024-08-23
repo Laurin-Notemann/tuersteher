0b2907e763ad309d12f37064b91adec165b9a031
Laurin-Notemann <laurin.notemann@gmail.com>
2024-08-23 17:47:41 +0200

    feat: add helper functions for oauth2 library

--- go.mod
+++ go.mod
@@ -2,6 +2,12 @@ module github.com/laurin-notemann/tuersteher
 
 go 1.22.3
 
-require golang.org/x/crypto v0.26.0
+require (
+	golang.org/x/crypto v0.26.0
+	golang.org/x/oauth2 v0.22.0
+)
 
-require golang.org/x/sys v0.23.0 // indirect
+require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+)
---
+++ oauth.go
@@ -0,0 +1,101 @@
+package tuersteher
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/oauth2"
+
+	"golang.org/x/oauth2/google"
+)
+
+type TuersteherOauth struct {
+	Cfg      *oauth2.Config
+	State    string
+	Userinfo string
+}
+
+type OauthOptions struct {
+	ClientId     string
+	ClientSecret string
+	RedirectUrl  string
+	Scopes       OauthScopes
+}
+
+type OauthScopes = []string
+
+type TuersteherUser struct {
+	Email string `json:"email"`
+}
+
+// Example scope:
+// []string{"https://www.googleapis.com/auth/userinfo.email"}
+func GetGoogleOauthConfig(options OauthOptions) *oauth2.Config {
+	return &oauth2.Config{
+		RedirectURL:  options.RedirectUrl,
+		ClientID:     options.ClientId,
+		ClientSecret: options.ClientSecret,
+		Scopes:       append([]string{"openid"}, options.Scopes...),
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func NewGoogleTuersteherOauth(options OauthOptions) (TuersteherOauth, error) {
+	state, err := GenerateRandomString(32)
+	if err != nil {
+		return TuersteherOauth{}, err
+	}
+
+	return TuersteherOauth{
+		Cfg:      GetGoogleOauthConfig(options),
+		State:    state,
+		Userinfo: "https://www.googleapis.com/oauth2/v2/userinfo",
+	}, nil
+}
+
+func (t *TuersteherOauth) GetAuthUrl() string {
+	return t.Cfg.AuthCodeURL(t.State)
+}
+
+func (t *TuersteherOauth) GetUserInfo(r *http.Request) (TuersteherUser, error) {
+	var user TuersteherUser
+
+	queryParams, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		return user, fmt.Errorf("Could not parse Query Params from Request: %s", err.Error())
+	}
+
+	state := queryParams.Get("state")
+	code := queryParams.Get("code")
+
+	if state != t.State {
+		return user, fmt.Errorf("Invalid OAuth state: %s", state)
+	}
+
+	token, err := t.Cfg.Exchange(r.Context(), code)
+	if err != nil {
+		return user, fmt.Errorf("Could not exhange code for token: %s", err.Error())
+	}
+
+	client := t.Cfg.Client(r.Context(), token)
+	res, err := client.Get(t.Userinfo)
+	if err != nil {
+		return user, fmt.Errorf("Failed to get user info (possible issue might be that the scope is not added): %s", err.Error())
+	}
+	defer res.Body.Close()
+
+	resByte, err := io.ReadAll(res.Body)
+	if err != nil {
+		return user, fmt.Errorf("Could not parse the body of the google api response: %s", err.Error())
+	}
+
+	err = json.Unmarshal(resByte, &user)
+	if err != nil {
+		return user, fmt.Errorf("Could not parse the bytes of the body: %s", err.Error())
+	}
+
+	return user, nil
+}
